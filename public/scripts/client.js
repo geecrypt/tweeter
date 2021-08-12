019fbf726096e27ee10eff30451b3ad3940c4a3d
geecrypt <gavina352@gmail.com>
2021-08-11 17:55:55 -0700

    dynamically add tweets using jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,25 +5,60 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const tweetsTestData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {"text": "If I have seen further it is by standing on the shoulders of giants"},
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 const createTweetElement = tweetData => {
+  const $header = $('<header>');
+  $header.append(`<div><img src=${tweetData.user.avatars}><h8>${tweetData.user.name}</h8></div>`);
+  $header.append(`<h4>${tweetData.user.handle}</h4>`);
+  const $p = $('<p>').text(tweetData.content.text);
+  // eslint-disable-next-line
+  const $timeCreated = $('<h6>').text(timeago.format(tweetData.created_at));
+  const $actionIcons = [
+    $('<i>').addClass('fas fa-flag'),
+    $('<i>').addClass('fas fa-retweet'),
+    $('<i>').addClass('fas fa-heart')
+  ];
 
+  const $footer = $('<footer>');
+  $footer.append($timeCreated, $('<div>').append(...$actionIcons));
+  const $tweet = $('<article>').addClass('tweet');
+  $tweet.append($header, $p, $footer);
+
+  console.log($tweet);
+  
+  return $tweet;
 };
 
-$(document).ready(function() {
-  // const $tweet = createTweetElement(tweetData);
-  const $tweet = $(`<article class="tweet"><h1>Hello world</h1></article>`);
+const renderTweets = tweetsData => {
+   for (const tweetData of tweetsData) {
+    $('#tweets-container').append(createTweetElement(tweetData));
+   }
+}
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+$(() => {
+  renderTweets(tweetsTestData);
 });
